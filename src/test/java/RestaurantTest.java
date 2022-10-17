7d7e28af71f24f2ac2ce2f1b14efddea8329cdfa
nksgr <nikitha.sagar55@gmail.com>
2022-10-17 15:52:24 +0530

    Part 2: Solution

---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,79 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTest {
+    Restaurant restaurant;
+
+    @BeforeEach
+
+    public void setup(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+    }
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        LocalTime mockCurrentTime = LocalTime.parse("12:00:00");
+        Restaurant spiedRestaurant = Mockito.spy(restaurant);
+        Mockito.when(spiedRestaurant.getCurrentTime()).thenReturn(mockCurrentTime); //mocks as 12pm
+        assertTrue(spiedRestaurant.isRestaurantOpen());
+
+        Mockito.when(spiedRestaurant.getCurrentTime()).thenReturn(mockCurrentTime.plusHours(5)); //mocks as 5pm
+        assertTrue(spiedRestaurant.isRestaurantOpen());
+
+        Mockito.when(spiedRestaurant.getCurrentTime()).thenReturn(mockCurrentTime.minusHours(1)); //mocks as 11am
+        assertTrue(spiedRestaurant.isRestaurantOpen());
+
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        LocalTime mockCurrentTime = LocalTime.parse("23:30:00");
+        Restaurant spiedRestaurant = Mockito.spy(restaurant);
+        Mockito.when(spiedRestaurant.getCurrentTime()).thenReturn(mockCurrentTime);
+        assertFalse(spiedRestaurant.isRestaurantOpen());
+
+        Mockito.when(spiedRestaurant.getCurrentTime()).thenReturn(mockCurrentTime.plusHours(4)); //mocks as 3:30am
+        assertFalse(spiedRestaurant.isRestaurantOpen());
+
+        Mockito.when(spiedRestaurant.getCurrentTime()).thenReturn(mockCurrentTime.minusHours(1)); // mocks as 10:30pm
+        assertFalse(spiedRestaurant.isRestaurantOpen());
+
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+}
